510b1acdf80edd331a7b46b43b796ba683c4ec73
akash-dev-234 <akash221002@gmail.com>
2024-11-12 23:31:37 +0530

    added get api for user and feed

--- src/app.js
+++ src/app.js
@@ -2,8 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from server");
+a; // Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
 });
 
 app.listen(3000, () => {
